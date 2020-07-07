31ae91b280297bb51b4d0057f20788b41972f8bb
Ganesh Sharma <sosukegani.tmk@gmail.com>
2020-07-07 20:20:55 +0530

    Ahalya

--- ahalya/urls.py
+++ ahalya/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("",include("hospitals.urls"))
+    path("", admin.site.urls),
+    path("api",include("hospitals.urls"))
 ]
