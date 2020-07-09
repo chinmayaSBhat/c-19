8ceb22d54e84f1c6af195fc4acd69279914b96f6
Ganesh Sharma <sosukegani.tmk@gmail.com>
2020-07-09 18:04:11 +0530

    All equipped-url

--- hospitals/urls.py
+++ hospitals/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "hospitals"
 
 urlpatterns = [
-    path("hospitalList/", views.HospitalDetailedList.as_view(), name="hospital_list"),
-    path("hospitalDetail/", views.HospitalDetailedSingle.as_view(), name="hospital_read"),
+    path("hospitalList", views.HospitalDetailedList.as_view(), name="hospital_list"),
+    path("hospitalDetail", views.HospitalDetailedSingle.as_view(), name="hospital_read"),
 
 ]
